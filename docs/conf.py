42e870a2c7c82c6361549c9acca1e2bd5093b505
David Gilman <davidgilman1@gmail.com>
2020-05-27 22:00:08 -0400

    Clean up and reformat docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -200,5 +200,5 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "oauth2client": ("https://oauth2client.readthedocs.io/en/latest/", None),
     "gspread": ("https://gspread.readthedocs.io/en/latest/", None),
-    "requests": ("http://docs.python-requests.org/en/master/", None),
+    "requests": ("https://requests.readthedocs.io/en/master/", None),
 }
