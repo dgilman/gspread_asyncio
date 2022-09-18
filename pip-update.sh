56d670772e94b3889c95b825013f901544172334
David Gilman <davidgilman1@gmail.com>
2022-09-18 09:48:10 -0400

    Update package versions

--- pip-update.sh
+++ pip-update.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-pip-compile requirements.in --output-file requirements.txt
-pip-compile requirements.in requirements_dev.in docs/requirements.in --output-file requirements_dev.txt
-pip-compile docs/requirements.in --output-file docs/requirements.txt
+pip-compile -U requirements.in --output-file requirements.txt
+pip-compile -U requirements.in requirements_dev.in docs/requirements.in --output-file requirements_dev.txt
+pip-compile -U docs/requirements.in --output-file docs/requirements.txt
