caf94af5c1edf17d6a27cd7de214b6652e1a973c
David Gilman <davidgilman1@gmail.com>
2023-02-20 20:13:44 -0500

    Use backtracking resolver for pip-tools

--- pip-update.sh
+++ pip-update.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-pip-compile -U requirements.in --output-file requirements.txt
-pip-compile -U requirements.in requirements_dev.in docs/requirements.in --output-file requirements_dev.txt
-pip-compile -U docs/requirements.in --output-file docs/requirements.txt
+pip-compile --resolver=backtracking -U requirements.in --output-file requirements.txt
+pip-compile --resolver=backtracking -U requirements.in requirements_dev.in docs/requirements.in --output-file requirements_dev.txt
+pip-compile --resolver=backtracking -U docs/requirements.in --output-file docs/requirements.txt
